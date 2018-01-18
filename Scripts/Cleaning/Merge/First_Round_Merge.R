d504ceff7e2801ce1fa3d12de6dc2f5196eac359
Sylvia Klosin <sylviaklosin@gmail.com>
2018-01-17 16:04:04 -0800

    Adding first data cleaning file

---
+++ Scripts/Cleaning/Merge/First_Round_Merge.R
@@ -0,0 +1,39 @@
+#========================
+# First Round Merge 
+#========================
+# Started by Sylvia Klosin (sylviaklosin@gmail.com) on Jan 17th 2018  
+# In this document we will start 
+
+#========================
+# Section 0: Setup
+#========================
+
+#=========
+# 0.1 Import Packages
+#=========
+library(data.table)
+library(dplyr)
+library(here)
+
+#=========
+# 0.2 Set working directory 
+#=========
+setwd(here::here("RawData"))
+
+#=========
+# Importing data
+#=========
+
+patent <- fread("")
+
+#========================
+# Section 1: 
+#========================
+
+
+
+
+#========================
+# Section 2: 
+#========================
+
